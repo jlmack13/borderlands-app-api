a33fbbc5ac5bb5eaba12b3ffc52ac0aabc4ae29b
jlmack13 <jennimacklin@gmail.com>
2018-06-29 13:31:38 -0400

    add create method to loot controller

--- app/controllers/api/loots_controller.rb
+++ app/controllers/api/loots_controller.rb
@@ -4,4 +4,19 @@ class Api::LootsController < ApplicationController
         @loots = Loot.all
         render json: @loots
     end
+
+    def create
+        @loot = Loot.new(loot_params)
+        if @loot.save
+            render json: @loot
+        else
+            render json: { errors: { message: "Loot failed to save."}}
+        end
+    end
+
+    private
+    
+    def loot_params
+        params.require(:loot).permit(:name, :item_type, :manufacturer, :location, :drop)
+    end
 end
