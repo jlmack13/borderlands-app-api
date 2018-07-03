3653b344c298e478a22332545b665987a36de692
jlmack13 <jennimacklin@gmail.com>
2018-07-03 12:35:57 -0400

    added image_url attribute to model

--- db/migrate/20180623150431_create_loots.rb
+++ db/migrate/20180623150431_create_loots.rb
@@ -5,6 +5,7 @@ class CreateLoots < ActiveRecord::Migration[5.1]
       t.string :item_type
       t.string :manufacturer
       t.string :drop
+      t.string :image_url, default: "https://vignette.wikia.nocookie.net/borderlands/images/b/bd/BL1_Red_Weapon_Chest.jpg/revision/latest?cb=20180216153856"
       t.boolean :story_mode, default: :false
       t.integer :story_id
       t.string :story_quest
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 20180623150952) do
     t.string "item_type"
     t.string "manufacturer"
     t.string "drop"
+    t.string "image_url", default: "https://vignette.wikia.nocookie.net/borderlands/images/b/bd/BL1_Red_Weapon_Chest.jpg/revision/latest?cb=20180216153856"
     t.boolean "story_mode", default: false
     t.integer "story_id"
     t.string "story_quest"
