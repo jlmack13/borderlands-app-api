479ff2f7d2761f504560ca6d984de1f9dc8f7ed7
jlmack13 <jennimacklin@gmail.com>
2018-07-27 13:31:39 -0400

    minor adjustment to work with rails 5

--- config/application.rb
+++ config/application.rb
@@ -20,10 +20,10 @@ module BorderlandsAppApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
